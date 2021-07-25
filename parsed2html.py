90cf4ebd6692a690d926cb76c7a4539bebb1727c
AviFS <avifsternlieb@gmail.com>
2021-07-25 11:36:36 -0700

    First line of text & default css working

---
+++ parsed2html.py
@@ -0,0 +1,13 @@
+f = open("parsed.txt", "r")
+while line:=f.readline():
+    if line[:2] != '//':
+        # print(line, end='')
+        splitter = line.split('"')
+        if len(splitter) == 3:
+            _, token, kind = splitter
+        kind = kind.strip(':')
+        kind = kind.strip(' ')
+        kind = kind.strip('\n')
+        print(f'<span class="{kind}">{token}</span>')
+
+f.close() 
