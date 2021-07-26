35690a67ff7418d06dfbcf14f0af33573a10ef8a
AviFS <avifsternlieb@gmail.com>
2021-07-26 01:23:27 -0700

    Cleaned up folder structure

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function init() {
-    return;
-}
-
-function run() {
-    var code = document.getElementById("input").value;
-    document.getElementById("output").innerHTML = interp(code, "abcd");
-}
-
 function changeStyle() {
     var lang = document.getElementById("lang-select").value;
     console.log(lang);
