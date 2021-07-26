e0e2b493f73158618b20f225ccf82b45bf2c3dfe
AviFS <avifsternlieb@gmail.com>
2021-07-26 15:44:07 -0700

    Added @rak1507's idea, issue #6.

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function changeStyle() {
-    var lang = document.getElementById("lang-select").value;
-    console.log(lang);
+function init() {
+    if (window.location.hash) {
+        var theme = window.location.hash.slice(1);
+        document.getElementById('lang-select').value = theme;
+        changeStyle();
+    }
+}
+
+function changeStyle(lang) {
+    if (!lang) { lang = document.getElementById("lang-select").value; }
     document.getElementById("output").className = "normal "+lang;
 }
 
