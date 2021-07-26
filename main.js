ed4ebc38de5045cf503e0172e5029bd094c95f04
AviFS <avifsternlieb@gmail.com>
2021-07-26 15:45:48 -0700

    Renamed: lang-select should be theme-select

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 function init() {
     if (window.location.hash) {
         var theme = window.location.hash.slice(1);
-        document.getElementById('lang-select').value = theme;
+        document.getElementById('theme-select').value = theme;
         changeStyle();
     }
 }
 
-function changeStyle(lang) {
-    if (!lang) { lang = document.getElementById("lang-select").value; }
-    document.getElementById("output").className = "normal "+lang;
+function changeStyle(theme) {
+    if (!theme) { theme = document.getElementById("theme-select").value; }
+    document.getElementById("output").className = "normal "+theme;
 }
 
 document.addEventListener("DOMContentLoaded", function(e) {
-   document.getElementById("lang-select").addEventListener("keydown", function(event) {
+   document.getElementById("theme-select").addEventListener("keydown", function(event) {
        if (event.keyCode == 13) {
            changeStyle();
        }
