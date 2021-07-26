0849088bba751174608858d3c4fd8726b9ffb3cd
AviFS <avifsternlieb@gmail.com>
2021-07-25 23:12:04 -0700

    Working prototype! Can now select between themes

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ function run() {
     var code = document.getElementById("input").value;
     document.getElementById("output").innerHTML = interp(code, "abcd");
 }
+
+function changeStyle() {
+    var lang = document.getElementById("lang-select").value;
+    console.log(lang);
+    document.getElementById("output").className = "normal "+lang;
+}
